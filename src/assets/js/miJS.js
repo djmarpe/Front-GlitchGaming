862e38f2d3d17933f31d3e626e6366a155adf1a5
Alejandro Martín <alejandro.martin.perez.99@gmail.com>
2021-05-05 19:57:06 +0200

    Correcciones en el menu desplegable, boceto de ultimos torneos

--- src/assets/js/miJS.js
+++ src/assets/js/miJS.js
@@ -1,23 +1,26 @@
 function comprobarMedia() {
     if (window.matchMedia("(min-width: 992px)").matches) {
-      console.log('Mas de 992px');
-      document.getElementById('padre').classList.remove('media-movil')
-      cargarMediaPC();
+        console.log('Mas de 992px');
+        //document.getElementById('menu-icon-close').click();
+        document.getElementById('lista').classList.remove('desplegar');
+        document.getElementById('lista').classList.remove('contraer');
+        cargarMediaPC();
     } else {
-      console.log('Menos de 992px');
-      document.getElementById('padre').classList.remove('media-pc')
-      cargarMediaMovil();
+        console.log('Menos de 992px');
+        document.getElementById('padre').classList.remove('media-pc');
+        document.getElementById('lista').classList.add('desplegar');
+        cargarMediaMovil();
     }
-  }
-  
-  function cargarMediaPC() {
+}
+
+function cargarMediaPC() {
     document.getElementById('padre').classList.remove('media-movil');
     document.getElementById('padre').classList.add('media-pc');
-  }
-  
-  function cargarMediaMovil() {
+}
+
+function cargarMediaMovil() {
     document.getElementById('padre').classList.remove('media-pc');
     document.getElementById('padre').classList.add('media-movil');
-  }
-  window.addEventListener('load', comprobarMedia);
-  window.addEventListener('resize', comprobarMedia);
+}
+window.addEventListener('load', comprobarMedia);
+window.addEventListener('resize', comprobarMedia);
